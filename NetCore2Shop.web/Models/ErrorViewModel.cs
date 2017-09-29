f0a8a2922eba3ae8ead96acffb9c26c84b2245c0
Blcile <jinguanbiao@outlook.com>
2017-09-29 13:32:14 +0800

    create project
    net core project created with docker

---
+++ NetCore2Shop.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCore2Shop.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
